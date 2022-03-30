71e1cd9dfda697c674b7852987f044276aea5e78
Cyberarm <matthewlikesrobots@gmail.com>
2022-03-30 16:30:56 -0500

    Reorganize file structure, use BattleViewEntity struct

---
+++ BattleView/include/BattleView.h
@@ -0,0 +1,78 @@
+/*	Renegade Scripts.dll
+	Copyright 2013 Tiberian Technologies
+
+	This file is part of the Renegade scripts.dll
+	The Renegade scripts.dll is free software; you can redistribute it and/or modify it under
+	the terms of the GNU General Public License as published by the Free
+	Software Foundation; either version 2, or (at your option) any later
+	version. See the file COPYING for more details.
+	In addition, an exemption is given to allow Run Time Dynamic Linking of this code with any closed source module that does not contain code covered by this licence.
+	Only the source code to the module(s) containing the licenced code has to be released.
+*/
+#pragma once
+
+#include "gmplugin.h"
+
+#define BATTLEVIEW_PLUGIN_VERSION 0.0f
+
+struct BattleViewPlayer
+{
+    int player_id;
+    int entity_id;
+    const char* username;
+};
+
+struct BattleViewEntity
+{
+    int id;
+    int team;
+    const char* name;
+    Vector3 position;
+    const char* type;
+    double health;
+    const char* icon; // Handle on BattleView Frontend?
+    const char* character_name;
+    const char* vehicle_name;
+};
+
+class BattleViewPlugin : public Plugin
+{
+    // Settings
+    int m_config_mode;
+    bool m_config_bots_as_players;
+
+    // Think Limiter
+    int m_think_interval;
+    int m_think_counter;
+
+    // TODO: Why angry?
+    // VectorClass<BattleViewPlayer> m_battleview_players;
+    // VectorClass<BattleViewEntity> m_battleview_entities;
+
+public:
+    BattleViewPlugin();
+
+    virtual ~BattleViewPlugin();
+
+    void HandleINIConfiguration(INIClass* ssgm, bool is_map);
+
+    virtual void OnLoadGlobalINISettings(INIClass* ssgm_ini);
+
+    virtual void OnFreeData();
+
+    virtual void OnLoadMapINISettings(INIClass* ssgm_ini);
+
+    virtual void OnFreeMapData();
+
+    virtual bool OnChat(int player_id, TextMessageEnum type, const wchar_t* message, int receiver_id);
+
+    virtual void OnLoadLevel();
+
+    virtual void OnGameOver();
+
+    virtual void OnPlayerJoin(int player_id, const char* player_name);
+
+    virtual void OnPlayerLeave(int player_id);
+
+    virtual void OnThink();
+};
---
+++ BattleView/src/BattleView.cpp
@@ -0,0 +1,212 @@
+/*	Renegade Scripts.dll
+	Copyright 2013 Tiberian Technologies
+
+	This file is part of the Renegade scripts.dll
+	The Renegade scripts.dll is free software; you can redistribute it and/or modify it under
+	the terms of the GNU General Public License as published by the Free
+	Software Foundation; either version 2, or (at your option) any later
+	version. See the file COPYING for more details.
+	In addition, an exemption is given to allow Run Time Dynamic Linking of this code with any closed source module that does not contain code covered by this licence.
+	Only the source code to the module(s) containing the licenced code has to be released.
+*/
+
+#include "General.h"
+#include "Defines.h"
+
+#include "include/BattleView.h"
+
+#include "GameObjManager.h"
+#include "BuildingGameObj.h"
+#include "BuildingGameObjDef.h"
+#include "SoldierGameObj.h"
+#include "VehicleGameObj.h"
+
+#include "scripts.h"
+#include "engine_tt.h"
+#include "engine_io.h"
+#include "gmgame.h"
+
+BattleViewPlugin::BattleViewPlugin() {
+    Console_Output("[BattleView] Loading cyberarm's BattleView Plugin v%d\n", BATTLEVIEW_PLUGIN_VERSION);
+    RegisterEvent(EVENT_GLOBAL_INI, this);
+    RegisterEvent(EVENT_MAP_INI, this);
+    RegisterEvent(EVENT_CHAT_HOOK, this);
+    RegisterEvent(EVENT_LOAD_LEVEL_HOOK, this);
+    RegisterEvent(EVENT_GAME_OVER_HOOK, this);
+    RegisterEvent(EVENT_PLAYER_JOIN_HOOK, this);
+    RegisterEvent(EVENT_PLAYER_LEAVE_HOOK, this);
+    RegisterEvent(EVENT_THINK_HOOK, this);
+
+    m_think_interval = 500; // 60;
+    m_think_counter = m_think_counter; // Run at first call
+}
+
+BattleViewPlugin::~BattleViewPlugin() {
+    Console_Output("[BattleView] Unloading cyberarm's BattleView Plugin v%d\n", BATTLEVIEW_PLUGIN_VERSION);
+    UnregisterEvent(EVENT_GLOBAL_INI, this);
+    UnregisterEvent(EVENT_MAP_INI, this);
+    UnregisterEvent(EVENT_CHAT_HOOK, this);
+    UnregisterEvent(EVENT_LOAD_LEVEL_HOOK, this);
+    UnregisterEvent(EVENT_GAME_OVER_HOOK, this);
+    UnregisterEvent(EVENT_PLAYER_JOIN_HOOK, this);
+    UnregisterEvent(EVENT_PLAYER_LEAVE_HOOK, this);
+    UnregisterEvent(EVENT_THINK_HOOK, this);
+}
+
+/*************
+* HELPER METHODS
+*/
+
+void BattleViewPlugin::HandleINIConfiguration(INIClass* ssgm_ini, bool is_map)
+{
+    m_config_mode            = ssgm_ini->Get_Int ("BattleView", "mode",            is_map ? m_config_mode : 0); // BattleView Frontend is currently fixed at mode 7
+    m_config_bots_as_players = ssgm_ini->Get_Bool("BattleView", "bots_as_players", is_map ? m_config_bots_as_players : true); // Appease SkyNET
+}
+
+
+/*************************
+* Load Global Settings
+*/
+void BattleViewPlugin::OnLoadGlobalINISettings(INIClass *ssgm_ini) {
+    Console_Output(__FUNCTION__ "\n");
+    Console_Output("[BattleView] Attempting to load global INI settings...\n");
+
+    HandleINIConfiguration(ssgm_ini, false);
+}
+
+// Don't need? What do?
+void BattleViewPlugin::OnFreeData() {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+// Configure Mode level
+void BattleViewPlugin::OnLoadMapINISettings(INIClass *ssgm_ini) {
+    Console_Output("[BattleView] Attempting to load map INI settings...\n");
+
+    HandleINIConfiguration(ssgm_ini, true);
+}
+
+void BattleViewPlugin::OnFreeMapData() {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+// Maybe handle configuration chat commands?
+bool BattleViewPlugin::OnChat(int player_id, TextMessageEnum type, const wchar_t *message, int receiver_id) {
+    Console_Output(__FUNCTION__ "\n");
+
+    const char* message_type = "UNKNOWN";
+
+    switch (type)
+    {
+    case TEXT_MESSAGE_PUBLIC:
+        message_type = "PUBLIC";
+        break;
+    case TEXT_MESSAGE_TEAM:
+        message_type = "TEAM";
+        break;
+    case TEXT_MESSAGE_PRIVATE:
+        message_type = "PRIVATE";
+        break;
+    case TEXT_MESSAGE_TMSG:
+        message_type = "TMSG";
+        break;
+    default:
+        break;
+    }
+
+    Console_Output("[%s] (%i) %s wrote: %s [%i]\n", message_type, player_id, "?sender_name?", message, receiver_id);
+    return true;
+}
+
+// TODO: Cache current server and map name
+void BattleViewPlugin::OnLoadLevel() {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+void BattleViewPlugin::OnGameOver() {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+void BattleViewPlugin::OnPlayerJoin(int player_id, const char *player_name) {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+void BattleViewPlugin::OnPlayerLeave(int player_id) {
+    Console_Output(__FUNCTION__ "\n");
+}
+
+// This will probably be our favorite callback
+void BattleViewPlugin::OnThink() {
+    m_think_counter += 1;
+
+    if (m_think_counter < m_think_interval)
+        return;
+
+    m_think_counter = 0;
+
+    Console_Output(__FUNCTION__ "\n");
+
+    Console_Output("Buildings:\n");
+    for (SLNode<BuildingGameObj>* z = GameObjManager::BuildingGameObjList.Head(); z; z = z->Next()) {
+        BuildingGameObj* obj = z->Data();
+
+        Vector3 pos;
+
+        obj->Get_World_Position(pos);
+        double health = 100.0; // (Commands->Get_Health(soldier) / Commands->Get_Max_Health(soldier)) * 100.0;
+
+        bool base_defense = obj->Get_Definition().Get_Type() == BuildingConstants::BuildingType::TYPE_BASE_DEFENSE;
+
+        BattleViewEntity entity;
+        entity.id = obj->Get_ID();
+        entity.team = obj->Get_Player_Type();
+        entity.name = Get_Translated_Preset_Name(obj);
+        entity.position = pos;
+        entity.type = base_defense ? "BASE_DEFENSE" : "BUILDING"; // BUILDING, BASE_DEFENSE
+        entity.health = health;
+        entity.icon = "";
+        entity.character_name = nullptr;
+        entity.vehicle_name = nullptr; // soldier->Get_Vehicle() != nullptr ? (const char*)soldier->Get_Vehicle()->Get_Vehicle_Name() : nullptr;
+
+        Console_Output(
+            "ID: %i, TEAM: %i, NAME: %s, POSITION: [%0.2f, %0.2f, %0.2f], TYPE: %s, HEALTH: %0.2f, ICON: %s, CHAR: %s, VEHICLE: %s\n",
+            entity.id, entity.team, entity.name, entity.position.X, entity.position.Y, entity.position.Z, entity.type, entity.health, entity.icon, entity.character_name, entity.vehicle_name
+        );
+
+        // Console_Output("Name: %s, Prefix: %s at X: %f,Y: %f, Z: %f for Team ID %i\n", Get_Translated_Preset_Name(building), building->Get_Name_Prefix(), pos.X, pos.Y, pos.Z, building->Get_Player_Type());
+    }
+
+    Console_Output("Players:\n");
+    for (SLNode<SoldierGameObj>* z = GameObjManager::SoldierGameObjList.Head(); z; z = z->Next()) {
+        SoldierGameObj* obj = z->Data();
+        Vector3 pos;
+
+        if (obj->Is_Bot() && !m_config_bots_as_players)
+            continue;
+
+        obj->Get_World_Position(pos);
+        double health = 100.0; // (Commands->Get_Health(soldier) / Commands->Get_Max_Health(soldier)) * 100.0;
+
+        BattleViewEntity entity;
+        entity.id = obj->Get_ID();
+        entity.team = obj->Get_Player_Type();
+        entity.name = Get_Player_Name(obj);
+        entity.position = pos;
+        entity.type = "SOLDIER"; // SOLDIER, VEHICLE, AIRCRAFT, [HELICOPTER?], HARVESTER
+        entity.health = health;
+        entity.icon = "";
+        entity.character_name = Get_Translated_Preset_Name(obj);
+        entity.vehicle_name = nullptr; // soldier->Get_Vehicle() != nullptr ? (const char*)soldier->Get_Vehicle()->Get_Vehicle_Name() : nullptr;
+
+        Console_Output(
+            "ID: %i, TEAM: %i, NAME: %s, POSITION: [%0.2f, %0.2f, %0.2f], TYPE: %s, HEALTH: %0.2f, ICON: %s, CHAR: %s, VEHICLE: %s\n",
+            entity.id, entity.team, entity.name, entity.position.X, entity.position.Y, entity.position.Z, entity.type, entity.health, entity.icon, entity.character_name, entity.vehicle_name
+        );
+    }
+}
+
+BattleViewPlugin battleViewPlugin;
+
+extern "C" __declspec(dllexport) Plugin *Plugin_Init() {
+    return &battleViewPlugin;
+}
